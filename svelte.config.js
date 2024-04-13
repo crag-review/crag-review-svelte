048d3eb74ee2f88c3ec8afbcac0dc6e46aaf0e0e
Benjamin Wieser <benjamin.wieser.ext@porscheinformatik.com>
2024-04-13 11:39:41 +0200

    404 error

--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: process.env.BASE_PATH+'/404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
