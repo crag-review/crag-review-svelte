689cda4ac32cfcab9957fa03dee50cd8a8021d61
Benjamin Wieser <benjamin.wieser.ext@porscheinformatik.com>
2024-04-13 11:52:31 +0200

    base

--- src/routes/blog/category/page/[page]/+page.server.js
+++ src/routes/blog/category/page/[page]/+page.server.js
@@ -1,7 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import { postsPerPage } from '$lib/config'
 import fetchPosts from '$lib/assets/js/fetchPosts'
-import { base } from '$service-worker';
+import { base } from '__sveltekit/paths';
 
 export const load = async ({ fetch, params }) => {
   const page = parseInt(params.page) || 1
