b468b46608d73cd50437956efc8d7cfc23982aa2
Benjamin Wieser <benjamin.wieser@wieser-engineering.xyz>
2024-06-15 15:35:33 +0200

    add mainpage content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,14 @@
 	<title>{siteTitle}</title>
 </svelte:head>
 
+<h1 class="text-4xl font-extrabold mt-4 ...">Klettergärten rund um Wien</h1>
+In diesem Blog sammle ich Klettergebiete rund um Wien, die gut mit öffentlichen Verkehrsmitteln erreichbar sind.<br>
+
+Sehr empfehlenswert ist auch der Mobilitätsguide des Alpenvereins: <a href="https://www.alpenverein.at/portal/natur-umwelt/sanfte_mobilitaet/Mobilitaetsguide-Sportklettern.php">Alpenverein</a><br>
+
+Klettergärten: <a href="/blog/category/sportklettern">Link</a><br>
+
+Mehrseillängen: <a href="/blog/category/Mehrseillängen">Link</a>
 
 <Accordion>
 	<AccordionItem open>
